e1531a3de3c7b4e157c7bc8418d35037d2a381b2
Barbara Mattioly <bmattiolyandrade@gmail.com>
2024-08-25 22:40:45 -0300

    feat(code): Create analysis for requisite 7

--- code/stats.py
+++ code/stats.py
@@ -1,16 +1,17 @@
 import csv
-
 from enums.MostPopularLanguages import MostPopularLanguages
 
-def median(lst):
-    number_of_elements = len(lst)
-    sorted_values = sorted(lst)
-    middle_element = number_of_elements  // 2
+# Calcula o valor mediano da lista recebida
+def calculate_median(list):
+    number_of_elements = len(list)
+    sorted_values = sorted(list)
+    middle_element = number_of_elements // 2
     if number_of_elements % 2 == 0:
         return (sorted_values[middle_element - 1] + sorted_values[middle_element]) / 2
     else:
         return sorted_values[middle_element]
 
+# Percorre o arquivo csv e coleta os dados para análise
 def analyze_data_from_csv(file_path):
     # Inicializa variáveis para armazenar os valores necessários para cada RQ
     total_age_days = []
@@ -21,13 +22,23 @@ def analyze_data_from_csv(file_path):
     total_issues = []
     closed_issues = []
 
-    data_for_analysis_by_language = {}
+    language_count = {language.value: 0 for language in MostPopularLanguages}
+
+    popular_languages_data = {
+        'pull_requests': [],
+        'releases': [],
+        'time_since_last_update': []
+    }
+
+    other_languages_data = {
+        'pull_requests': [],
+        'releases': [],
+        'time_since_last_update': []
+    }
 
     # Lê o arquivo CSV
     with open(file_path, mode="r", newline="", encoding="utf-8") as file:
         reader = csv.DictReader(file)
-
-        language_count = {language.value: 0 for language in MostPopularLanguages}
 
         for row in reader:
             # Converte e coleta os dados necessários para a análise
@@ -46,32 +57,32 @@ def analyze_data_from_csv(file_path):
             primary_language = row["Linguagem primaria"]
             primary_languages.append(primary_language)
 
+            closed_issue = int(row["Total de Issues Fechadas"])
+            total_issue = int(row["Total de Issues"])
+
+            closed_issues.append(closed_issue)
+            total_issues.append(total_issue)
+
             if primary_language in language_count:
                 language_count[primary_language] += 1
-
-            closed_issues.append(int(row["Total de Issues Fechadas"]))
-            total_issues.append(int(row["Total de Issues"]))
-
-            if primary_language not in data_for_analysis_by_language:
-                data_for_analysis_by_language[primary_language] = {
-                    'pull_requests': [],
-                    'releases': [],
-                    'time_since_last_update': []
-                }
-            data_for_analysis_by_language[primary_language]['pull_requests'].append(pull_requests)
-            data_for_analysis_by_language[primary_language]['releases'].append(releases)
-            data_for_analysis_by_language[primary_language]['time_since_last_update'].append(time_since_update_days)
+                popular_languages_data['pull_requests'].append(pull_requests)
+                popular_languages_data['releases'].append(releases)
+                popular_languages_data['time_since_last_update'].append(time_since_update_days)
+            else:
+                other_languages_data['pull_requests'].append(pull_requests)
+                other_languages_data['releases'].append(releases)
+                other_languages_data['time_since_last_update'].append(time_since_update_days)
 
     # Faz os cálculos e exibe os resultados para cada requisito
     results = {
-        'RQ01_median_age': median(total_age_days),
-        'RQ02_median_pull_requests': median(total_pull_requests),
-        'RQ03_median_releases': median(total_releases),
-        'RQ04_median_time_since_update': median(time_since_last_update),
+        'RQ01_median_age': calculate_median(total_age_days),
+        'RQ02_median_pull_requests': calculate_median(total_pull_requests),
+        'RQ03_median_releases': calculate_median(total_releases),
+        'RQ04_median_time_since_update': calculate_median(time_since_last_update),
         'RQ05_popular_languages_percentage': analyze_popular_languages(primary_languages),
         'RQ05_amount_of_repositories_per_language': language_count,
-        'RQ06_closed_issues_percentage': calculate_closed_issues_percentage(median(closed_issues), median(total_issues)),
-        'RQ07_analysis_by_language': analyze_data_by_language(data_for_analysis_by_language)
+        'RQ06_closed_issues_percentage': calculate_closed_issues_percentage(calculate_median(closed_issues), calculate_median(total_issues)),
+        'RQ07_analysis_by_language': analyze_data_by_language(popular_languages_data, other_languages_data)
     }
 
     return results
@@ -92,20 +103,25 @@ def analyze_popular_languages(primary_languages):
 
     return (popular_language_count / quantity) * 100
 
-# REQ06 - Função para calcular o percentual de issues fechadas, calculado através da razão dos valores medianos de issues fechadas e total de issues dos repositórios
+# REQ06 - Função para calcular o percentual de issues fechadas, através da razão dos valores medianos de issues fechadas e total de issues dos repositórios
 def calculate_closed_issues_percentage(closed_issues, total_issues):
     closed_issues_percentage =(closed_issues / total_issues) * 100 if total_issues > 0 else 0
     return round(closed_issues_percentage, 2)
 
-# REQ07 - Função para calcular
-def analyze_data_by_language(data_by_language):
-    analysis_results = {}
-    for language, data in data_by_language.items():
-        analysis_results[language] = {
-            'median_pull_requests': median(data['pull_requests']),
-            'median_releases': median(data['releases']),
-            'median_time_since_last_update': median(data['time_since_last_update'])
+# REQ07 - Função para obter a mediana de pull requests, releases e tempo desde a última atualização para repositórios com linguagens populares e outras linguagens
+def analyze_data_by_language(popular_data, other_data):
+    analysis_results = {
+        'popular_languages': {
+            'median_pull_requests': calculate_median(popular_data['pull_requests']),
+            'median_releases': calculate_median(popular_data['releases']),
+            'median_time_since_last_update': calculate_median(popular_data['time_since_last_update'])
+        },
+        'other_languages': {
+            'median_pull_requests': calculate_median(other_data['pull_requests']),
+            'median_releases': calculate_median(other_data['releases']),
+            'median_time_since_last_update': calculate_median(other_data['time_since_last_update'])
         }
+    }
     return analysis_results
 
 # Função principal para rodar o script e imprimir os resultados
@@ -128,8 +144,12 @@ if __name__ == "__main__":
     print(f"\n• RQ06 - Percentual de Issues Fechadas: {analysis_results['RQ06_closed_issues_percentage']}")
 
     print("\n• RQ07 - Análise por Linguagem:")
-    for language, data in analysis_results['RQ07_analysis_by_language'].items():
-        print(f"Linguagem: {language}")
-        print(f"  Mediana de Pull Requests: {data['median_pull_requests']}")
-        print(f"  Mediana de Releases: {data['median_releases']}")
-        print(f"  Mediana do Tempo desde a Última Atualização (dias): {data['median_time_since_last_update']}")
+    print("          - Linguagens Populares:")
+    print(f"            - Mediana de Pull Requests: {analysis_results['RQ07_analysis_by_language']['popular_languages']['median_pull_requests']}")
+    print(f"            - Mediana de Releases: {analysis_results['RQ07_analysis_by_language']['popular_languages']['median_releases']}")
+    print(f"            - Mediana do Tempo desde a Última Atualização (dias): {analysis_results['RQ07_analysis_by_language']['popular_languages']['median_time_since_last_update']}")
+    
+    print("\n          - Outras Linguagens:")
+    print(f"            - Mediana de Pull Requests: {analysis_results['RQ07_analysis_by_language']['other_languages']['median_pull_requests']}")
+    print(f"            - Mediana de Releases: {analysis_results['RQ07_analysis_by_language']['other_languages']['median_releases']}")
+    print(f"            - Mediana do Tempo desde a Última Atualização (dias): {analysis_results['RQ07_analysis_by_language']['other_languages']['median_time_since_last_update']}")
